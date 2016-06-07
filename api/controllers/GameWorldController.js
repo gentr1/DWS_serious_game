7c8bdc3244544f4fa976b31f8f77575c9b2eb7e8
gentr1 <mehdi.khoury@gmail.com>
2016-06-07 10:05:04 +0100

    working version of the eater distribution system serious game
    Needs node.js and a mongodb server to be installed and running.
    Then do npm install -g sails
    Then go to folder and do npm install .
    Then type sails lift to run server.
    You can play the game if you open Chrome at localhost:1337

---
+++ api/controllers/GameWorldController.js
@@ -0,0 +1,88 @@
+/**
+ * GameWorldController
+ *
+ * @description :: Server-side logic for managing gameworlds
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	reset: function (req, res) {
+		if (!req.isSocket) {return res.badRequest();}
+		GameWorld.find().limit(1).exec( function foundGame(err, game) {
+			if (err) return res.negotiate(err);
+			if (!game) return res.notFound();
+			
+			var gameWorldObj = {
+			  //id: game.id,
+			  overallBest: {cost:game[0].overallBest.cost},
+			  sessionBests: []
+			}
+			GameWorld.update(game.id, gameWorldObj, function worldUpdated(err2) {
+				if (err2) return res.negotiate(err2);
+			});
+		});
+	},
+	update: function (req, res) {
+		if (!req.isSocket) {return res.badRequest();}
+		
+		GameWorld.find().limit(1).exec( function foundGame(err, game) {
+			if (err) return res.negotiate(err);
+			if (!game) return res.notFound();
+			//console.log("updating high score")
+			//console.log(req.body)
+			if (req.body['newscore']){
+				var cost= req.body['newscore'][0];
+				var pressure_deficit= req.body['newscore'][1];
+				//if sessionBests
+				if ( (cost< game[0].overallBest.cost) && (pressure_deficit>=0)){
+					if (game[0].sessionBests.length==0){
+						//var newscores= game[0].sessionBests;
+						//if (newscores[0] && newscores[0]['score'] < cost)
+						var newscores=[];
+						newscores.push({'score':cost, 'user':req.body['user']});
+						var gameWorldObj = {
+						  //id: game.id,
+						  overallBest: {cost:game[0].overallBest.cost},
+						  sessionBests: newscores
+						}
+						GameWorld.update(game.id, gameWorldObj, function worldUpdated(err2) {
+							if (err2) return res.negotiate(err2);
+							sails.sockets.broadcast('funSockets', 'betterscore', {score:cost, user:req.body['user']});
+						});
+					}
+					else{
+						if (game[0].sessionBests[0] && cost < game[0].sessionBests[0]['score']){
+							var newscores=game[0].sessionBests;
+							newscores.unshift({'score':cost, 'user':req.body['user']});
+							var gameWorldObj = {
+							  //id: game.id,
+							  overallBest: {cost:game[0].overallBest.cost},
+							  sessionBests: newscores
+							}
+							GameWorld.update(game.id, gameWorldObj, function worldUpdated(err2) {
+								if (err2) return res.negotiate(err2);
+								sails.sockets.broadcast('funSockets', 'betterscore', {score:cost, user:req.body['user']});
+							});
+						}
+					}
+					//console.log("world "+game[0].overallBest.cost);
+				}
+				//console.log("updating high score from "+req.body['user']+ ": "+ cost)
+				//if (cost< game[0].overallBest.cost && cost< pressure_deficit>=0){
+					
+					
+				//}
+			}
+		});
+	},
+	ending: function (req, res) {
+		if (!req.isSocket) {return res.badRequest();}
+		
+		GameWorld.find().limit(1).exec( function foundGame(err, game) {
+			if (err) return res.negotiate(err);
+			if (!game) return res.notFound();
+			sails.sockets.broadcast('funSockets', 'end', {overallBest: game[0].overallBest,sessionBests: game[0].sessionBests});
+		});
+	},
+};
+
